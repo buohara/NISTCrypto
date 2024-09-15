24b9c202ee0589faac14762e9c17263d08b93a93
Ben O'Hara <buohara@gmail.com>
2024-09-15 17:54:01 -0400

    Add diffing functionality for computing database deltas and compressing them.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
-
 #include "primes.h"
 #include "bigint.h"
+#include "diff.h"
 
 int main(int argc, char** argv)
 {
@@ -9,20 +9,35 @@ int main(int argc, char** argv)
     6962155154859963260211100482357357666900094513013513488352858667799199787495340476167566639530574848375895722792291996203873323650274512138128403360943634134259376986501375967452208380337012919869885380406071772232795575963202558402893589313281327208179913789760736615950818685956393838601277519011418885197723428318400763080858914698836058070301404903262955501113318317950597435778777212408626799143
     */
 
-    /*BigInt a("32168516513513", 10);
-    printf("BigInt a in decimal: %s\n", a.GetDecimalString().c_str());
+    string m1 = "Once upon a time, in a galaxy far, far away.";
+    vector<uint8_t> s1;
+    StringToByteVec(m1, s1);
+    
+    string m2 = "Once upon a time, in a magnificent galaxy far away.";
+    vector<uint8_t> s2;
+    StringToByteVec(m2, s2);
 
-    BigInt b("111010100000111010000111111111110111011101001", 2);
-    printf("BigInt b in binary: %s\n", b.GetBinaryString().c_str());
+    vector<uint8_t> lcs;
+    vector<Diff> diffs;
 
-    BigInt c("1D41D0FFEEE9", 16);
-    printf("BigInt c in hex: %s\n", c.GetHexString().c_str());*/
+    LCS(s1, s2, lcs, diffs);
 
-    BigInt n("6962155154859963260211100482357357666900094513013513488352858667799199787495340476167566639530574848375895722792291996203873323650274512138128403360943634134259376986501375967452208380337012919869885380406071772232795575963202558402893589313281327208179913789760736615950818685956393838601277519011418885197723428318400763080858914698836058070301404903262955501113318317950597435778777212408626799143", 10);
-    BigInt p1;
-    BigInt p2;
+    string lcsStr;
+    ByteVecToString(lcs, lcsStr);
 
-    FactorSemiPrimeNearestSquaresBigInt(n, p1, p2);
+    printf("LCS = %s\n\n", lcsStr.c_str());
+
+    for (uint64_t i = 0; i < diffs.size(); i++)
+    {
+        string d1Str;
+        string d2Str;
+
+        ByteVecToString(diffs[i].d1, d1Str);
+        ByteVecToString(diffs[i].d2, d2Str);
+
+        printf("LCS diff %llu at LCS[%llu]: d1 = '%s', d2 = '%s'\n",
+            i, diffs[i].offset, d1Str.c_str(), d2Str.c_str());
+    }
 
     return 0;
 }
